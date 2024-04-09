617d3c105fcd31a864014e4d9bc1d02d0ac52b11
rmontgo <122593115+rmontgo@users.noreply.github.com>
2024-04-08 19:42:54 -0600

    database migrations and UML

--- django_project/eventspace/forms.py
+++ django_project/eventspace/forms.py
@@ -2,8 +2,19 @@ from django import forms
 from django.forms import ModelForm
 from .models import Event
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
+
 
 class EventForm(ModelForm):
     class Meta:
         model = Event
-        fields = ['title', 'description','location', 'image']
+        fields = ['title', 'description', 'date', 'start', 'end', 'location', 'image']
+        widgets = {
+            "date": DateInput(),
+            "start": TimeInput(),
+            "end": TimeInput(),
+        }
--- django_project/eventspace/models.py
+++ django_project/eventspace/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+import datetime
+from django.utils import timezone
 # Create your models here.
 
 class Event(models.Model):
@@ -7,7 +9,9 @@ class Event(models.Model):
     location = models.CharField(max_length=200, blank=False)
     image = models.ImageField(blank=True)
     description = models.TextField(blank=True)
-
+    date = models.DateField(("Date"), default=datetime.date.today, blank=False)
+    start = models.TimeField(default=None, null=True, blank=True)
+    end = models.TimeField(default=None, null=True, blank=True)
     def __str__(self):
         return self.title
     def get_absolute_url(self):
